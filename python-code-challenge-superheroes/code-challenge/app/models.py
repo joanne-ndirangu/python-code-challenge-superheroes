44498e88c656583da577d3a11ffb950225d4d6d9
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-10-02 12:34:52 +0300

    Initial commit

---
+++ python-code-challenge-superheroes/code-challenge/app/models.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Hero(db.Model):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    powers = db.relationship('Power', backref='heroes')
+
+class Power(db.Model):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    heroes = db.relationship('Hero', backref='powers')
+
+
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    hero = db.relationship('Hero', backref='hero_powers')
+    power = db.relationship('Power', backref='power_heroes')
